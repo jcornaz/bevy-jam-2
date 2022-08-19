dc667f27e5ce1e1ef56ad3140963950a0fd7f1bc
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-08-19 20:29:36 +0200

    add bevy_prototype_lyon

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@
 #![warn(nonstandard_style, rust_2018_idioms)]
 
 use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
 
 #[cfg(feature = "bevy-inspector-egui")]
 use bevy_inspector_egui::prelude::*;
 
 fn main() {
     let mut app = App::new();
-    app.add_plugins(DefaultPlugins);
+    app.add_plugins(DefaultPlugins).add_plugin(ShapePlugin);
 
     #[cfg(feature = "bevy-inspector-egui")]
     app.add_plugin(WorldInspectorPlugin::default());
